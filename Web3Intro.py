4d83bffe162342081e9dedb0ecf7359df9811ace
lagnajitparida <lagnajit.parida1@gmail.com>
2023-06-19 12:26:11 -0700

    Test update

--- Web3Intro.py
+++ Web3Intro.py
@@ -3,24 +3,24 @@ from web3 import  Web3
 
 infura_url = "https://mainnet.infura.io/v3/596ca473ac0f403fa5e802b38d7da783"
 web3 = Web3(Web3.HTTPProvider(infura_url))
-print(web3.isConnected())
+print(web3.is_connected())
 
 
-print(web3.eth.blockNumber)
+print(web3.eth.block_number)
 
 eth_address = "0x1e0b908c935B9B121aE046A256E45781F56fa4d6"
 
-balance_in_wei = web3.eth.getBalance(eth_address) # gives result in Wei
+balance_in_wei = web3.eth.get_balance(eth_address) # gives result in Wei
 
 balance_in_ether = balance_in_wei / 10 ** 18
 
-balance_in_ether_2 = web3.fromWei(balance_in_wei, "ether") # gives result in ether
+balance_in_ether_2 = web3.from_wei(balance_in_wei, "ether") # gives result in ether
 
 
 
 
 
-
+# This is a test
 
 
 # # convert 1 ether to the lowest unit 'Wei'
